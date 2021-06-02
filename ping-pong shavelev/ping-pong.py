25d22c282ff7962f30a2bebd83d86f2487b9805a
Spark2oo7 <84630156+Spark2oo7@users.noreply.github.com>
2021-06-02 15:18:03 +0300

    перерисовал ракетки и добавил очки

--- ping-pong shavelev/ping-pong.py
+++ ping-pong shavelev/ping-pong.py
@@ -1,4 +1,4 @@
-#dy spark2oo7
+#dy Spark2oo7
 from pygame import *
 from random import randint
 
@@ -18,10 +18,11 @@ class GameSprite(sprite.Sprite):
 
 
 class Player(GameSprite):
-    def __init__(self, fileName, x, y, size_x, size_y, speed, namber):
+    def __init__(self, fileName, x, y, size_x, size_y, speed, namber, score):
         super().__init__(fileName, x, y, size_x, size_y, speed)
         self.recharge = 0
         self.namber = namber
+        self.score = score
     
     def update(self):
         if self.recharge != 0:
@@ -72,6 +73,13 @@ class Ball(GameSprite):
             win = 1
     
 
+def restart():
+    ball.dir_x = randint(0, 1)*2 - 1
+    ball.dir_y = randint(0, 1)*2 - 1
+    ball.rect.x = 300
+    ball.rect.y = 150
+    ball.speed = 5
+
 #создай окно игры
 win_width = 700
 win_height = 500
@@ -82,18 +90,19 @@ display.set_caption('Шутер')
 #задай фон сцены
 background = transform.scale(image.load('backgraund.png'), (win_width, win_height))
 
-player1 = Player("racket.png", 0, 150, 50, 200, 5, 1)
-player2 = Player("racket.png", 650, 150, 50, 200, 5, 2)
+player1 = Player("racket1.png", 0, 150, 50, 200, 5, 1, 0)
+player2 = Player("racket2.png", 650, 150, 50, 200, 5, 2, 0)
 
 ball = Ball("ball.png", 300, 150, 50, 50, 5, 1, 1)
 
 
 font.init()
 
-font = font.SysFont("Arial", 80)
+font1 = font.SysFont("Arial", 50)
+font2 = font.SysFont("Arial", 80)
 
-win1 = font.render("Игрок 1 победил!", 1, (255, 255, 0))
-win2 = font.render("Игрок 2 победил!", 1, (255, 255, 0))
+win1 = font2.render("Игрок 1 победил!", 1, (255, 255, 0))
+win2 = font2.render("Игрок 2 победил!", 1, (255, 255, 0))
 
 #игровой цикл
 win = 0
@@ -108,23 +117,43 @@ while game:
             game = False
 
     if not finish:
-        window.blit(background, (0, 0))
-
-        ball.update()
-        ball.reset()
-
         player1.update()
-        player1.reset()
         
         player2.update()
-        player2.reset()
+
+        ball.update()
 
         if win == 1:
-            window.blit(win1, (100, 200))
-            finish = True
+            player1.score += 1
+            restart()
+            win = 0
         if win == 2:
-            window.blit(win2, (100, 200))
+            player2.score += 1
+            restart()
+            win = 0
+        
+
+        window.blit(background, (0, 0))
+
+        score1 = font1.render(str(player1.score), 1, (255, 128, 0))
+        window.blit(score1, (100, 100))
+
+        score2 = font1.render(str(player2.score), 1, (0, 128, 128))
+        window.blit(score2, (win_width - 100, 100))
+
+        if player2.score >= 3:
             finish = True
+            window.blit(win2, (100, 200))
+
+        if player1.score >= 3:
+            finish = True
+            window.blit(win1, (100, 200))
+
+        ball.reset()
+        player1.reset()
+        player2.reset()
+
+        
     
     display.update()
     clock.tick(FPS)
